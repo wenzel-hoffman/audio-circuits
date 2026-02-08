b90eddb9ac41e83c76025e08616e94d1590d2777
Wenzel Hoffman <wenzel.hoffman@gmail.com>
2026-02-08 22:10:01 +0200

    Update nixpkgs pin 1cd347bf → 41e216c0

--- nix/nixpkgs.pin.nix
+++ nix/nixpkgs.pin.nix
@@ -3,9 +3,9 @@
 
 let
   # Branch: nixos-25.11
-  # Date: 2026-01-26T11:59:14Z
-  commit = "1cd347bf3355fce6c64ab37d3967b4a2cb4b878c";
-  sha256 = "sha256:195pkrjal51mr7v6psjvx1ap3vcnyvp076kzngjfg4cgvskplg1j";
+  # Date: 2026-02-01T19:41:40Z-01-26T11:59:14Z
+  commit = "41e216c0ca66c83b12ab7a98cc326b5db01db646";
+  sha256 = "sha256:084zixgy2k5rscg3m65fmmw6k9za9g8wpbp63b0y9h787n1fdci3";
   url = "https://github.com/NixOS/nixpkgs/archive/${commit}.tar.gz";
 in
 
