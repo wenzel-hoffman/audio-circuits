b101ba9e497d92da864a0e8a66c38dc0713e13a9
Wenzel Hoffman <wenzel.hoffman@gmail.com>
2026-02-14 03:13:13 +0200

    Fix open-schematic.sh (no *.diy files and single file opening)

--- dev-scripts/open-schematic.sh
+++ dev-scripts/open-schematic.sh
@@ -16,7 +16,7 @@ set -o errexit || exit; set -o errtrace; set -o nounset; set -o pipefail
 
 SCRIPT_DIR=$(dirname -- "${BASH_SOURCE[0]}")
 
-SCHEMATIC_FILES=$(ls ./*.{kicad_sch,diy})
+SCHEMATIC_FILES=$(shopt -s nullglob; ls ./*.{kicad_sch,diy})
 readarray -t SCHEMATIC_FILES_ARR <<< "$SCHEMATIC_FILES"
 
 open-file() (
@@ -32,12 +32,12 @@ open-file() (
     fi
 
     set -o xtrace
-    </dev/null &>/dev/null "$run_diylc" "$selected_file" & disown
+    </dev/null &>/dev/null "$run_diylc" "$FILE" & disown
 
   # KiCad schematic file
   elif [[ $FILE == *.kicad_sch ]]; then
     set -o xtrace
-    </dev/null &>/dev/null eeschema -- "$selected_file" & disown
+    </dev/null &>/dev/null eeschema -- "$FILE" & disown
 
   else
     >&2 printf "Unrecognized file format: %s\n" "$FILE"
@@ -57,10 +57,7 @@ if (( ${#SCHEMATIC_FILES_ARR[@]} > 1 )); then
     >&2 echo 'Not a valid selection! Try again.'
   done
 elif (( ${#SCHEMATIC_FILES_ARR[@]} == 1 )); then
-  (
-    set -o xtrace
-    open-file "${SCHEMATIC_FILES_ARR[0]}"
-  )
+  open-file "${SCHEMATIC_FILES_ARR[0]}"
 else
   # Should not really happen, `ls` would fail to expand glob earlier.
   >&2 echo '[ERROR] No schematic files were found!'
